2bdde032f17f8e7059f74ef2e6d795a88d8b77a9
cool168 <cool168@users.noreply.github.com>
2016-07-19 11:17:44 +0800

    Update xware.sh

--- xware.sh
+++ xware.sh
@@ -3,9 +3,6 @@
 INSTALL_FLAG="/app/xware.installed"
 
 if [ ! -f "$INSTALL_FLAG" ]; then
-	dpkg -i /app/libc6_*.deb
-	dpkg -i /app/libc6-i386_*.deb
-	dpkg -i /app/lib32z1_*.deb
 	chmod +x /app/xware/portal
 	touch $INSTALL_FLAG
 fi
