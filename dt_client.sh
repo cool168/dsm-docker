7600d752cfc26c57ffb498735d6692437ecd58dd
cool168 <cool168@users.noreply.github.com>
2016-08-05 20:21:53 +0800

    Update dt_client.sh

--- dt_client.sh
+++ dt_client.sh
@@ -4,4 +4,11 @@ echo ${LOCAL_PORT='443'}
 echo ${PASSWD='test'}
 echo ${ACT_PORT='127.0.0.1:443'}
 echo ${XOR_CODE='1234123'}
-/usr/bin/dtunnel_lite -service $SERVER_PORT -local $LOCAL_PORT -auth $PASSWD -encrypt -action $ACT_PORT -xor $XOR_CODE -r
+echo ${RMODE='yes'}
+
+if [ $RMODE = "yes" ]
+then
+  /usr/bin/dtunnel_lite -service $SERVER_PORT -local $LOCAL_PORT -auth $PASSWD -encrypt -action $ACT_PORT -xor $XOR_CODE -r
+else
+  /usr/bin/dtunnel_lite -service $SERVER_PORT -local $LOCAL_PORT -auth $PASSWD -encrypt -action $ACT_PORT -xor $XOR_CODE
+fi
