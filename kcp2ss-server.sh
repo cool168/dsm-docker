2a72bc01ff61b9c030d59c4b48c0fdab4e591db6
cool168 <cool168@users.noreply.github.com>
2016-09-25 20:35:31 +0800

    Update kcp2ss-server.sh

--- kcp2ss-server.sh
+++ kcp2ss-server.sh
@@ -11,7 +11,7 @@ echo ${TARGET_PORT='127.0.0.1:12948'}
 
 echo ${MODE='fast2'}
 	
-echo ${MTU=1350}
+echo ${MTU=1400}
 	
 echo ${SNDWND=1024}
 	
@@ -19,9 +19,15 @@ echo ${RCVWND=1024}
 	
 echo ${CRYPT='none'}
 	
-echo ${KEY='cuteribs'}
+echo ${KEY='cool168'}
+
+echo ${SS_SERVER_PORT=9090}
+
+echo ${SS_SERVER_METHOD='aes-256-cfb'}
+
+echo ${SS_SERVER_PWD='cool168'}
 
 sleep 1
-ss-server -s 0.0.0.0 -p 9090 -m chacha20 -k $PWD1 -t 60 -u -A >/dev/null 2>&1 &
+ss-server -s 0.0.0.0 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t 60 -u -A >/dev/null 2>&1 &
 sleep 5
-/app/kcp/server_linux_amd64 -l $KCP_PORT -t 127.0.0.1:9090 -mode fast2 -nocomp -mtu 1400 -sndwnd 2048 -rcvwnd 2048 -crypt salsa20 -key $PWD2 >/dev/null 2>&1
+/app/kcp/server_linux_amd64 -l $KCP_PORT -t 127.0.0.1:$SS_SERVER_PORT -mode $MODE -nocomp -mtu $MTU -sndwnd $SNDWND -rcvwnd 2048 -crypt salsa20 -key $PWD2 >/dev/null 2>&1
