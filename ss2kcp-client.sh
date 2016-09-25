39e95c8bf4e927a074c0edb2f22eb984d1c17aa5
cool168 <cool168@users.noreply.github.com>
2016-09-25 20:57:10 +0800

    Create ss2kcp-client.sh

---
+++ ss2kcp-client.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+INSTALL_FLAG="/app/ss2kcp-client.installed"
+
+if [ ! -f "$INSTALL_FLAG" ]; then
+	touch $INSTALL_FLAG
+fi
+
+echo ${LOCAL_PORT=':12948'}
+
+echo ${KCP_PORT='vps:39900'}
+
+echo ${MODE='fast2 -nocomp -autoexpire 60'}
+	
+echo ${MTU=1400}
+	
+echo ${SNDWND=128}
+	
+echo ${RCVWND=1024}
+	
+echo ${CRYPT='salsa20'}
+	
+echo ${KEY='cool168'}
+
+echo ${CONN=1}
+
+echo ${DSCP=46}
+
+sleep 1
+
+sleep 5
+/app/kcp/client_linux_amd64 -l $LOCAL_PORT -r $KCP_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY -conn $CONN -dscp $DSCP
+
