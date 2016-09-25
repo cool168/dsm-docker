7b6d0042ae70745a21b5143cce83c2f414ee1f27
cool168 <cool168@users.noreply.github.com>
2016-09-25 21:14:51 +0800

    Update ss2kcp-client.sh

--- ss2kcp-client.sh
+++ ss2kcp-client.sh
@@ -6,9 +6,9 @@ if [ ! -f "$INSTALL_FLAG" ]; then
 	touch $INSTALL_FLAG
 fi
 
-echo ${LOCAL_PORT=':12948'}
+echo ${LOCAL_PORT=12948}
 
-echo ${KCP_PORT='vps:39900'}
+echo ${KCP_PORT='vps:29900'}
 
 echo ${MODE='fast2 -nocomp -autoexpire 60'}
 	
@@ -26,8 +26,12 @@ echo ${CONN=1}
 
 echo ${DSCP=46}
 
-sleep 1
+echo ${SS_LOCAL_PORT=8989}
 
+echo ${SS_SERVER_METHOD='aes-256-cfb'}
+
+sleep 1
+ss-local -s 127.0.0.1 -p $LOCAL_PORT -b 0.0.0.0 -l $SS_LOCAL_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t 60 -u -A &
 sleep 5
-/app/kcp/client_linux_amd64 -l $LOCAL_PORT -r $KCP_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY -conn $CONN -dscp $DSCP
+/app/kcp/client_linux_amd64 -l :$LOCAL_PORT -r $KCP_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY -conn $CONN -dscp $DSCP
 
