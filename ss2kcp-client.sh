c22b66105263d41b1a570f8bf5058bba498991ca
cool168 <cool168@users.noreply.github.com>
2016-09-29 17:48:45 +0800

    Update ss2kcp-client.sh

--- ss2kcp-client.sh
+++ ss2kcp-client.sh
@@ -33,7 +33,12 @@ echo ${SS_LOCAL_PORT=8989}
 echo ${SS_SERVER_METHOD='aes-256-cfb'}
 
 sleep 1
-/app/kcp/client_linux_amd64 -l 127.0.0.1:$LOCAL_PORT -r $KCP_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY -conn $CONN -dscp $DSCP &
+if [ $SHOW_LOGS = "yes" ]
+then
+    /app/kcp/client_linux_amd64 -l 127.0.0.1:$LOCAL_PORT -r $KCP_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY -conn $CONN -dscp $DSCP &
+else
+    /app/kcp/client_linux_amd64 -l 127.0.0.1:$LOCAL_PORT -r $KCP_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY -conn $CONN -dscp $DSCP >/dev/null 2>&1 &
+fi
 sleep 5
 ss-local -s 127.0.0.1 -p $LOCAL_PORT -b 0.0.0.0 -l $SS_LOCAL_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t 60 -u -A
 
