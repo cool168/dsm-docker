c57288146a49e8a6258f9c25336134e5aa0a2949
cool168 <cool168@users.noreply.github.com>
2017-03-04 15:11:07 +0800

    Add env SS_UDP and SS_OTA

--- ss-server.sh
+++ ss-server.sh
@@ -6,11 +6,15 @@ if [ ! -f "$INSTALL_FLAG" ]; then
 	touch $INSTALL_FLAG
 fi
 
+echo ${SS_UDP="-u"}
+
+echo ${SS_OTA=""}
+
 sleep 1
 
 if [ $ENABLE_CFGFILE = "yes" ]
 then
 	ss-server -u -c /etc/ss-config.json
 else
-	ss-server -s 0.0.0.0 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t 60 -u -A
+	ss-server -s 0.0.0.0 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t 60 $SS_UDP $SS_OTA
 fi
