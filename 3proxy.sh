79da66cce970b7253a3ef61a6142fc25006e783e
cool168 <cool168@users.noreply.github.com>
2017-04-08 22:14:10 +0800

    Update 3proxy.sh

--- 3proxy.sh
+++ 3proxy.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
+echo ${EN_3CFG='no'}
+
 INSTALL_FLAG="/app/3proxy.installed"
 CONF="/app/proxycfg/3proxy.conf"
-
-if [ ! -f "$INSTALL_FLAG" ]; then
-	chmod +x /app/3proxy
+if  [ $EN_3CFG = "no" ]
+then
+	if [ ! -f "$INSTALL_FLAG" ]; then
+		chmod +x /app/3proxy
 	
-	echo ${PORT=1080}
-	echo "nscache 65536" > $CONF
-	echo "timeouts 1 5 30 60 180 1800 15 60" >> $CONF
-	echo "auth none" >> $CONF
-	echo "socks -p$PORT" >> $CONF
+		echo ${PORT=1080}
+		echo "nscache 65536" > $CONF
+		echo "timeouts 1 5 30 60 180 1800 15 60" >> $CONF
+		echo "auth none" >> $CONF
+		echo "socks -p$PORT" >> $CONF
 	
-	touch $INSTALL_FLAG
+		touch $INSTALL_FLAG
+	fi
 fi
-
 sleep 1
 /app/3proxy $CONF
