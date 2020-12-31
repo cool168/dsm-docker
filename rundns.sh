30a44dcc1eac44842347cf689320ab884ff4b9de
cool168 <cool168@users.noreply.github.com>
2020-12-31 16:24:03 +0800

    Update rundns.sh

--- rundns.sh
+++ rundns.sh
@@ -1,1 +1,6 @@
-./dns2socks 127.0.0.1:1080 8.8.8.8:53 127.0.0.1:53 >/dev/null
+#!/bin/sh
+echo ${SERVER_PORT='127.0.0.1:1080'}
+echo ${FORWORD_DNS='8.8.4.4:53'}
+echo ${LOCAL_DNS='0.0.0.0:5353'}
+
+./dns2socks $SERVER_PORT $FORWORD_DNS $LOCAL_DNS >/dev/null
